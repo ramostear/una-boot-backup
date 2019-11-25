b856114f506db766c598e446526d796fd7d68df6
ramostear <ramostear@163.com>
2019-11-25 16:19:32 +0800

    加入清理缓存数据的定时任务

--- src/main/java/com/ramostear/unaboot/UnaBootApplication.java
+++ src/main/java/com/ramostear/unaboot/UnaBootApplication.java
@@ -1,11 +1,21 @@
 package com.ramostear.unaboot;
 
+import com.ramostear.unaboot.repository.support.UnaRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableCaching
+@EnableScheduling
+@EnableJpaAuditing
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class},
+                       scanBasePackages = "com.ramostear.unaboot")
+@EnableJpaRepositories(basePackages = {"com.ramostear.unaboot.repository"},
+                       repositoryBaseClass = UnaRepositoryImpl.class)
 public class UnaBootApplication {
 
     public static void main(String[] args) {
