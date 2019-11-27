ca1a09baad2c0cb0b6ab0a8f2619581fdc0b92b8
ramostear <ramostear@163.com>
2019-11-27 21:05:25 +0800

    修复访问量统计错误

--- src/main/java/com/ramostear/unaboot/web/FrontController.java
+++ src/main/java/com/ramostear/unaboot/web/FrontController.java
@@ -193,7 +193,7 @@ public class FrontController extends UnaController{
             ehcache.remove(post.getId()+"_visits");
             return post.getVisits();
         }else{
-            return post.getVisits()+count;
+            return (post.getVisits())+1;
         }
 
     }
